db8e2e2c4d3afb89b2381447a13c886fb5b7db08
IvandryPage <ivandry.89@gmail.com>
2025-03-20 09:03:33 +0700

    Add gitignore and intro to c++ concepts

--- template_function.cpp
+++ template_function.cpp
@@ -1,4 +1,5 @@
 // Template function for repetitive function
+#include <concepts>
 #include <cstring>
 #include <iostream>
 // the bad way
@@ -46,6 +47,25 @@ const char *maximum<const char *>(const char *a, const char *b);
 template <>
 int maximum<int>(int a, int b);
 
+// C++ CONCEPTS
+/*
+    same_as
+    derived_from
+    convertible_to
+    common_reference_with
+    common_with
+    integral
+    signed_integral
+    unsigned_integral
+    floating_point
+ */
+
+template <typename T>
+  requires std::integral<T>
+T add(T a, T b) {
+  return a + b;
+}
+
 int main() {
   float x{8.5f};
   float y{5.5f};
@@ -80,6 +100,11 @@ int main() {
   std::cout << "Out - &a: " << &y << std::endl;
   sum(y, x);
   std::cout << "Out - &a: " << &y << std::endl;
+
+  // CONCEPTS C++
+  char a_0{10};
+  char a_1{20};
+  auto result_a = add(a_0, a_1);
   return 0;
 }
 
