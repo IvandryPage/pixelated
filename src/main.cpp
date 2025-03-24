56c74dd418fb2824aaa0183268645a6fc0a8d09e
IvandryPage <ivandry.89@gmail.com>
2025-03-24 09:56:50 +0700

    Add texts

--- src/main.cpp
+++ src/main.cpp
@@ -2,10 +2,11 @@
 #include <iostream>
 #include <list>
 
-constexpr float kGravity = 980.0f;
 constexpr float kGroundLevel = 500.0f;
 std::array<float, 3> HeightLevel{400.0f, 250.0f, 100.0f};
 size_t current_level{0};
+bool game_over{false};
+bool is_running{true};
 
 sf::RenderWindow window;
 sf::RectangleShape player;
@@ -18,6 +19,10 @@ float spawn_time{2.0f};
 float speed_up_cooldown{10.0f};
 int speed_level{0};
 float obstacle_speed{-300.0f};
+size_t score{0};
+
+sf::Font font("assets/ShareTechMono-Regular.ttf");
+std::vector<sf::Text> texts;
 
 void initialize();
 void events();
@@ -28,10 +33,13 @@ sf::RectangleShape initializePlayer();
 sf::RectangleShape initializeGround(float width);
 
 int main() {
+  srand(time(0));
   initialize();
   while (window.isOpen()) {
     events();
-    update();
+    if (is_running) {
+      update();
+    }
     render();
   }
 }
@@ -43,10 +51,30 @@ void initialize() {
 
   player = initializePlayer();
   ground = initializeGround(window.getSize().x * 1.0f);
+
+  texts.push_back(sf::Text(font, "Score  : " + std::to_string(score), 24));
+  texts.push_back(
+      sf::Text(font, "Speed+ : " + std::to_string(speed_up_cooldown), 24));
+  texts.push_back(sf::Text(font, "Spawn  : " + std::to_string(spawn_time), 24));
+  texts.push_back(
+      sf::Text(font, "Obstacles  : " + std::to_string(obstacles.size()), 24));
+
+  texts.push_back(sf::Text(font, "GAME OVER!\nPress Escape to exit!", 30));
+  texts.push_back(
+      sf::Text(font, "Press Up and Down arrow on keyboard to move", 30));
+
+  texts[0].setPosition({100, 700});
+  texts[1].setPosition({100, 800});
+  texts[2].setPosition({100, 900});
+  texts[3].setPosition({400, 700});
+  texts[4].setPosition({600, 800});
+  texts[5].setPosition({100, 500});
 }
 
 void events() {
   delta_time = game_clock.restart().asSeconds();
+  obstacles.remove_if(
+      [](const sf::CircleShape& c) { return c.getPosition().x < 0; });
 
   while (std::optional event = window.pollEvent()) {
     if (event->is<sf::Event::Closed>()) {
@@ -59,7 +87,8 @@ void events() {
           window.close();
           break;
         case sf::Keyboard::Scan::Space:
-          speed_up_cooldown = 0.0f;
+          is_running = !is_running;
+          game_over = false;
           break;
         case sf::Keyboard::Scan::Up:
           if (current_level < HeightLevel.size() - 1) {
@@ -94,9 +123,20 @@ void update() {
   player.setPosition({200.0f, HeightLevel[current_level]});
   for (auto& obstacle : obstacles) {
     obstacle.move({obstacle_speed * delta_time, 0});
-    if (obstacle.getGlobalBounds().findIntersection(player.getGlobalBounds()))
-      window.close();
+    obstacle.rotate(sf::Angle(sf::degrees(-10.0f)));
+    if (obstacle.getGlobalBounds().findIntersection(player.getGlobalBounds())) {
+      game_over = true;
+      is_running = false;
+    }
+    if (player.getPosition().x > obstacle.getPosition().x) {
+      score++;
+    }
   }
+
+  texts[0].setString("Score  : " + std::to_string(score));
+  texts[1].setString("Speed+ : " + std::to_string(speed_up_cooldown));
+  texts[2].setString("Spawn  : " + std::to_string(spawn_time));
+  texts[3].setString("Obstacles  : " + std::to_string(obstacles.size()));
 }
 
 void render() {
@@ -106,6 +146,16 @@ void render() {
   for (auto& obstacle : obstacles) {
     window.draw(obstacle);
   }
+  for (auto& text : texts) {
+    if (text.getString().find("GAME OVER")) {
+      window.draw(text);
+    }
+  }
+
+  if (game_over) {
+    window.draw(texts[4]);
+  }
+
   window.display();
 }
 
@@ -148,7 +198,7 @@ void generateObstacle() {
 
   int point_count = rand() % 8 + 3;
 
-  sf::CircleShape obstacle(50);
+  sf::CircleShape obstacle(35);
   obstacle.setOrigin(obstacle.getGeometricCenter());
   obstacle.setPosition(
       {window.getSize().x + 100.0f, HeightLevel[height_level]});
