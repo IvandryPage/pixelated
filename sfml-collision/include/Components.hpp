1dd2818e56470f9590de35ae2644c7161cc44354
IvandryPage <ivandry.89@gmail.com>
2025-03-27 16:32:27 +0700

    Add sfml AABB collision

---
+++ sfml-collision/include/Components.hpp
@@ -0,0 +1,77 @@
+#pragma once
+#include <SFML/Graphics.hpp>
+
+#include "Vec2.hpp"
+
+struct Component {
+  bool exists{false};
+};
+
+struct Shape : public Component {
+  sf::CircleShape circle;
+  sf::RectangleShape rectangle;
+  Shape() = default;
+
+  Shape(Vec2<float> size, const sf::Color& outline) : rectangle(size) {
+    rectangle.setOutlineColor(outline);
+    rectangle.setOutlineThickness(10.0f);
+    rectangle.setFillColor(sf::Color::Transparent);
+    rectangle.setOrigin(rectangle.getGeometricCenter());
+  }
+
+  Shape(float radius, size_t points, sf::Color fill, sf::Color outline,
+        float thickness)
+      : circle(radius, points) {
+    circle.setFillColor(fill);
+    circle.setOutlineColor(outline);
+    circle.setOutlineThickness(thickness);
+    circle.setOrigin(Vec2<float>(radius, radius));
+  }
+};
+
+struct Transform : public Component {
+  Vec2<float> position{0, 0};
+  Vec2<float> velocity{0, 0};
+  sf::Angle angle;
+
+  Transform() = default;
+  Transform(const Vec2<float>& position, const Vec2<float>& velocity)
+      : position(position), velocity(velocity) {}
+};
+
+struct Collision : public Component {
+  float radius;
+  Vec2<float> size;
+  Vec2<float> half_size;
+  Vec2<float> overlap;
+
+  Collision() = default;
+  Collision(Vec2<float> vec)
+      : size(vec), half_size(vec.x / 2.0f, vec.y / 2.0f) {}
+  Collision(float radius) : radius(radius) {}
+};
+
+struct Input : public Component {
+  bool up{false};
+  bool down{false};
+  bool left{false};
+  bool right{false};
+  bool shoot{false};
+
+  Input() = default;
+};
+
+struct LifeSpan : public Component {
+  int total{0};
+  int remaining{0};
+  LifeSpan() = default;
+  LifeSpan(int total_life_span)
+      : total(total_life_span), remaining(total_life_span) {}
+};
+
+struct Score : public Component {
+  int score{0};
+
+  Score() = default;
+  Score(int score) : score(score) {}
+};
